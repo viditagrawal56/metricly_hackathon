abd35e725354e43c46771f97d99d9fd30c29cde5
aniirathod <aniketnr5023@gmail.com>
2025-01-08 23:21:52 +0530

    Analytics page design

---
+++ src/components/Graph/AnimatedText.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
+import "./Graphs.css";
+
+const texts: string[] = [
+  "Gain insights into your social media performance",
+  "Track your engagement across multiple platforms",
+  "Optimize your content strategy with data-driven decisions",
+  "Understand your audience better through analytics",
+];
+
+export const AnimatedText: React.FC = () => {
+  const [index, setIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 4000); // Change text every 4 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      {" "}
+      {/* Fixed height container */}
+      <AnimatePresence mode="wait">
+        <motion.p
+          key={index}
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -50, opacity: 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className="animated-text"
+        >
+          {texts[index]}
+        </motion.p>
+      </AnimatePresence>
+    </div>
+  );
+};
